abd3eb5a0b6968b6564536e0374ad595c35c1884
phudyka <paul.hudyka@gmail.com>
2023-11-14 09:43:14 +0100

    fix ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: phudyka <phudyka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 15:06:03 by phudyka           #+#    #+#             */
-/*   Updated: 2023/11/03 15:22:38 by phudyka          ###   ########.fr       */
+/*   Updated: 2023/11/14 09:03:39 by phudyka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,11 @@ int main(void)
 	std::string *stringPTR = &brain;
 	std::string &stringREF = brain;
  
-	std::cout << "string = "<< brain << std::endl;
+	std::cout << "string = "<< &brain << std::endl;
 	std::cout << "stringPTR = "<< stringPTR << std::endl;
-	std::cout << "stringREF = "<< stringREF << std::endl;
+	std::cout << "stringREF = "<< &stringREF << std::endl;
+	std::cout << "string value = "<< brain << std::endl;
+	std::cout << "stringPTR value = "<< *stringPTR << std::endl;
+	std::cout << "stringREF value = "<< stringREF << std::endl;
 	return (0);
-}
+} 
